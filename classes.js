1a70f6e3e4ec8dc50df3d731fa7d89172269d865
Christoph Lenz <coiz@hotmail.de>
2021-03-02 21:45:02 +0100

    Added Inheritance

--- classes.js
+++ classes.js
@@ -23,10 +23,9 @@ halley.incrementBehavior(); // Add one to behavior
 console.log(halley.name); // Print name value to console
 console.log(halley.behavior); // Print behavior value to console
 
-class Surgeon {
-  constructor(name, department) {
+class HospitalEmployee {
+  constructor(name) {
     this._name = name;
-    this._department = department;
     this._remainingVacationDays = 20;
   }
 
@@ -34,20 +33,57 @@ class Surgeon {
     return this._name;
   }
 
-  get department() {
-    return this._department;
-  }
-
   get remainingVacationDays() {
     return this._remainingVacationDays;
   }
 
   takeVacationDays(daysOff) {
-    const startingVacationDays = this.remainingVacationDays;
-    const leftOverVacationDays = startingVacationDays - daysOff;
-    this._remainingVacationDays = leftOverVacationDays;
+    this._remainingVacationDays -= daysOff;
+  }
+
+  static generatePassword() {
+    return Math.floor(Math.random() * 10000);
   }
 }
+
+class Nurse extends HospitalEmployee {
+  constructor(name, certifications) {
+    super(name);
+    this._certifications = certifications;
+  }
+
+  get certifications() {
+    return this._certifications;
+  }
+
+  addCertification(newCertification) {
+    this._certifications.push(newCertification);
+  }
+}
+
+
+class Surgeon extends HospitalEmployee {
+  constructor(name, department) {
+    super(name);
+    this._department = department;
+  }
+
+  get department() {
+    return this._department;
+  }
+
+  set department() {
+    return this._department;
+  }
+}
+
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics']);
+
+nurseOlynyk.takeVacationDays(5);
+console.log(nurseOlynyk.remainingVacationDays);
+
+nurseOlynyk.addCertification('Genetics');
+console.log(nurseOlynyk.certifications);
 
 const surgeonRomero = new Surgeon('Francisco Romero', 'Cardiovascular');
 const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics');
