763bfec8706681aa359e0557517b0c90491b84b4
Christoph Lenz <coiz@hotmail.de>
2021-03-03 22:29:11 +0100

    Added Optional Requirements of Library Project

--- library-classes.js
+++ library-classes.js
@@ -1,8 +1,41 @@
+class Catalog {
+  constructor() {
+    this._items = [];
+  }
+
+  get items() {
+    return this._items;
+  }
+
+  set items(items) {
+    this._items = items;
+  }
+
+  addMedia(media) {
+    this._items.push(media);
+  }
+
+  removeMedia(guid) {
+    let newItems = [];
+    this._items.forEach(item => {
+      if (item._guid !== guid) {
+        newItems.push(item);
+      };
+    });
+    this.items = newItems;
+  }
+}
+
 class Media {
   constructor(title) {
+    this._guid = Media.createGuid();
     this._title = title;
     this._isCheckedOut = false;
     this._ratings = [];
+  }
+
+  get guid() {
+    return this._guid;
   }
 
   get title() {
@@ -21,12 +54,29 @@ class Media {
     this._isCheckedOut = status;
   }
 
+  static createGuid() {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+  }
+
+
   toggleCheckoutStatus() {
     this.isCheckedOut = !this.isCheckedOut;
   }
 
   addRating(numberIn) {
-    this.ratings.push(numberIn);
+    let number = Number(numberIn);
+    if (isNaN(number)) {
+      console.log("Please provide a valid number between 1 and 5");
+    } else {
+      if (number >= 1 && number <= 5) {
+        this.ratings.push(numberIn);
+      } else {
+        console.log("Rating must be between 1 and 5");
+      }
+    }
   }
 
   getAverageRating() {
@@ -71,26 +121,72 @@ class Movie extends Media {
   }
 }
 
+class CD extends Media {
+  constructor(title, artist, songs) {
+    super(title);
+    this._artist = artist;
+    this._songs = songs;
+  }
+
+  get artist() {
+    return this._artist;
+  }
+
+  get songs() {
+    return this._songs;
+  }
+
+  shuffle() {
+    return this._songs[Math.floor(Math.random() * this._songs.length)];
+  }
+}
+
 const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544)
 
 historyOfEverything.toggleCheckoutStatus();
-console.log(historyOfEverything.isCheckedOut);
+// console.log(historyOfEverything.isCheckedOut);
 
 historyOfEverything.addRating(4);
 historyOfEverything.addRating(5);
 historyOfEverything.addRating(5);
-console.log(historyOfEverything.getAverageRating());
-
-console.log(historyOfEverything);
+historyOfEverything.addRating(10);
+historyOfEverything.addRating('A');
+// console.log(historyOfEverything.getAverageRating());
 
 const speed = new Movie('Jan de Bont', 'Speed', 116);
 
 speed.toggleCheckoutStatus();
-console.log(speed.isCheckedOut);
+// console.log(speed.isCheckedOut);
 
 speed.addRating(1);
 speed.addRating(1);
 speed.addRating(5);
-console.log(speed.getAverageRating());
+// console.log(speed.getAverageRating());
 
-console.log(speed);
+const theGreatWar = new CD(
+  'The Great War',
+  'Sabaton',
+  [
+    'The Future of Warfare',
+    'Seven Pillars of Wisdom',
+    '82nd All the Way',
+    'The Attack of the Dead Men',
+    'Devil Dogs',
+    'The Red Baron',
+    'Great War',
+    'A Ghost in the Trenches',
+    'Fields of Verdun',
+    'The End of the War to End All Wars',
+    'In Flanders Fields'
+  ]
+)
+
+console.log(theGreatWar.shuffle());
+
+const my_catalog = new Catalog();
+
+my_catalog.items = [historyOfEverything, speed, theGreatWar];
+// my_catalog.removeMedia(speed.guid);
+// console.log(my_catalog);
+// console.log(historyOfEverything);
+// console.log(speed);
