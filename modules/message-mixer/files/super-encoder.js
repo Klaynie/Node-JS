d0601d1fe70f465bd0a17def3052e5df47d7356a
Christoph Lenz <coiz@hotmail.de>
2021-04-25 17:41:29 +0200

    Added Message Mixer Project

--- modules/message-mixer/files/super-encoder.js
+++ modules/message-mixer/files/super-encoder.js
@@ -1,14 +1,14 @@
 // Import the encryptors functions here.
-
+const { caesarCipher, symbolCipher, reverseCipher } = require('./encryptors.js');
 
 const encodeMessage = (str) => {
   // Use the encryptor functions here.
-
+  return reverseCipher(symbolCipher(caesarCipher(str, 6)));
 }
 
 const decodeMessage = (str) => {
   // Use the encryptor functions here.
-
+  return caesarCipher(symbolCipher(reverseCipher(str)), -6);
 }
 
 // User input / output.
