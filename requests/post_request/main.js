4878081d60a3de55a66ec7b484918f2344ec8baa
Christoph Lenz <coiz@hotmail.de>
2021-04-27 14:17:22 +0200

    Added bytesize Code Snippets

--- requests/post_request/main.js
+++ requests/post_request/main.js
@@ -1,5 +1,7 @@
+import { API_KEY } from './key.js';
+
 // Information to reach API
-const apiKey = '<Your API Key>';
+const apiKey = API_KEY;
 const url = 'https://api.rebrandly.com/v1/links';
 
 // Some page elements
@@ -8,7 +10,21 @@ const shortenButton = document.querySelector('#shorten');
 const responseField = document.querySelector('#responseField');
 
 // AJAX functions
-const shortenUrl = () => {}
+const shortenUrl = () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({ destination: urlToShorten });
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      renderResponse(xhr.response);
+    }
+  };
+  xhr.open('POST', url);
+  xhr.setRequestHeader('Content-type', 'application/json');
+  xhr.setRequestHeader('apikey', apiKey);
+  xhr.send(data);
+}
 
 
 // Clear page and call AJAX functions
