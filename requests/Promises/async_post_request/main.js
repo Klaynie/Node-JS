ead9e2d58052fff7e20e23709faa6ef305b13420
Christoph Lenz <coiz@hotmail.de>
2021-04-28 10:35:37 +0200

    Finished async POST Exercise

--- requests/Promises/async_post_request/main.js
+++ requests/Promises/async_post_request/main.js
@@ -10,7 +10,26 @@ const shortenButton = document.querySelector('#shorten');
 const responseField = document.querySelector('#responseField');
 
 // AJAX functions
-// Code goes here
+const shortenUrl = async() => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({ destination: urlToShorten });
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      body: data,
+      headers: {
+        'Content-type': 'application/json',
+        'apikey': apiKey
+      }
+    });
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      renderResponse(jsonResponse);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 // Clear page and call AJAX functions
 const displayShortUrl = (event) => {
