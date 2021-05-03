6756ce4c18a81f59354a53902e02e054cb774e2d
Christoph Lenz <coiz@hotmail.de>
2021-05-03 17:13:49 +0200

    Added Interactivity Code Snippets

---
+++ d3/Interactivity/main.js
@@ -0,0 +1,18 @@
+let poemVerses = [
+  "Always",
+  "in the middle",
+  "of our bloodiest battles",
+  "you lay down your arms",
+  "like flowering mines",
+  "to conquer me home."
+];
+
+d3.select("#viz")
+  .selectAll("p")
+  .data(poemVerses)
+  .enter()
+  .append("p")
+  .text("Click Me!")
+  .on("click", function(d) {
+    d3.select(this).text(d);
+  });
