65b6e5bfaadd7c4d9e2851ae8260ad44446efa08
Christoph Lenz <coiz@hotmail.de>
2021-05-25 15:39:35 +0200

    Factory

--- patterns/index.js
+++ patterns/index.js
@@ -1,27 +1,26 @@
-let instance = null;
-
 class Car {
   constructor(doors, engine, color) {
-    if (!instance) {
-      this.doors = doors;
-      this.engine = engine;
-      this.color = color;
-      instance = this;
-    } else {
-      return instance;
+    this.doors = doors;
+    this.engine = engine;
+    this.color = color;
+  }
+}
+
+class carFactory {
+  createCar(type) {
+    switch (type) {
+      case 'civic':
+        return new Car(4, 'V6', 'grey');
+      case 'honda':
+        return new Car(4, 'V8', 'red');
     }
   }
 }
 
-// class SUV extends Car {
-//   constructor(doors, engine, color) {
-//     super(doors, engine, color);
-//     this.wheels = 4;
-//   }
-// }
 
-const civic = new Car(4, 'V6', 'grey');
-const honda = new Car(4, 'V8', 'red')
+const factory = new carFactory();
+const civic = factory.createCar('civic');
+const honda = factory.createCar('honda');
 
 console.log(civic);
 console.log(honda);
