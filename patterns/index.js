f87a073183de153964f2cdd3a113407e913bad45
Christoph Lenz <coiz@hotmail.de>
2021-05-25 15:44:41 +0200

    Abstract factory

--- patterns/index.js
+++ patterns/index.js
@@ -6,7 +6,7 @@ class Car {
   }
 }
 
-class carFactory {
+class CarFactory {
   createCar(type) {
     switch (type) {
       case 'civic':
@@ -17,10 +17,37 @@ class carFactory {
   }
 }
 
+class Suv {
+  constructor(doors, engine, color) {
+    this.doors = doors;
+    this.engine = engine;
+    this.color = color;
+  }
+}
 
-const factory = new carFactory();
-const civic = factory.createCar('civic');
-const honda = factory.createCar('honda');
+class SuvFactory {
+  createCar(type) {
+    switch (type) {
+      case 'cx4':
+        return new Car(4, 'V6', 'grey');
+      case 'cx5':
+        return new Car(4, 'V8', 'red');
+    }
+  }
+}
 
-console.log(civic);
-console.log(honda);
+const carfactory = new CarFactory();
+const suvFacotry = new SuvFactory();
+
+const autoManufacturer = (type, model) => {
+  switch (type) {
+    case 'car':
+      return carFactory.createCar(model);
+    case 'suv':
+      return suvFacotry.createCar(model);
+  }
+}
+
+const cx5 = autoManufacturer('suv', 'cx5');
+
+console.log(cx5);
